fb05043cc031637b421298a56efd2e39f265c9a6
Kleina13 <ragnarhelgi22@gmail.com>
2019-09-26 15:33:48 +0000

    i am understanding

--- app.py
+++ app.py
@@ -2,31 +2,38 @@
 
 from os import urandom
 from flask import Flask, render_template as rend, session
+# from pprint import pprint
 
 app = Flask(__name__)
 app.secret_key = urandom(13)
 
 @app.route('/')
 def index():
-	session['user'] = 'Palli'
+	session['cart'] = ["item", "another item"]
 	return '<h1>Velkominn</h1>'
+
+@app.route('/cart')
+def cart():
+	lepic = ""
+	for item in session['cart']:
+		lepic += " " + item
+	return f'<h2>{lepic}</h2><a href="/">back</a>'
 
 @app.route('/get')
 def get_session():
-	if 'user' in session:
-		return session['user']
-	return 'Not logged in'
+	if 'cart' in session:
+		return str(session['cart'])
+	return 'your cart is empty'
 
 @app.route('/kill')
 def kill_user_session():
-	session.pop('user', None)
-	return 'Killed Palli'
+	session.pop('cart', None)
+	return 'emptied your cart'
 
 # error <<<<<<<<<<
 @app.errorhandler(404)
 def error404(error):
 	return '<br><br><h1 style="text-align: center;">ERROR 404</h1><h2 style="text-align: center;">page not found<h2>'
-
 @app.errorhandler(500)
 def error500(error):
 	return '<br><br><h1 style="text-align: center;">ERROR 500</h1><h2 style="text-align: center;">page not found<h2>'
