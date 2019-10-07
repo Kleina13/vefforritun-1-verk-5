0cede3674a68992bf3f0e98de903791ce76dcc6d
Kleina13 <ragnarhelgi22@gmail.com>
2019-10-07 23:18:57 +0000

    so many much thing ok

--- app.py
+++ app.py
@@ -7,12 +7,20 @@ from flask import Flask, render_template as rend, session, request
 app = Flask(__name__)
 app.secret_key = urandom(13)
 
-books = [{'id': 0, 'name': "The Anarchist Cookbook", 'value': "34.99", 'image': "acb.jpg"},
-		 {'id': 1, 'name': "The Communist Manifesto", 'value': "21.99", 'image': "c_man.jpg"},
-		 {'id': 2, 'name': "Mein Kampf", 'value': "48.99", 'image': "mein_k.jpg"},
-		 {'id': 3, 'name': "Quran", 'value': "20.99", 'image': "Quran.jpg"},
-		 {'id': 4, 'name': "Lord of the Flies", 'value': "46.99", 'image': "lotf.jpg"},
-		 {'id': 5, 'name': "The Gospel of the Flying Spaghetti Monster", 'value': "37.99", 'image': "tgoftfsm.jpg"}]
+books = [{'id': 0, 'name': "The Anarchist Cookbook", 'value': "34.96", 'image': "acb.jpg"},
+		 {'id': 1, 'name': "The Communist Manifesto", 'value': "21.96", 'image': "c_man.jpg"},
+		 {'id': 2, 'name': "Mein Kampf", 'value': "48.96", 'image': "mein_k.jpg"},
+		 {'id': 3, 'name': "Quran", 'value': "20.96", 'image': "Quran.jpg"},
+		 {'id': 4, 'name': "Lord of the Flies", 'value': "46.96", 'image': "lotf.jpg"},
+		 {'id': 5, 'name': "The Gospel of the Flying Spaghetti Monster", 'value': "9.96", 'image': "tgoftfsm.jpg"},
+		 {'id': 6, 'name': "Angels of the Universe", 'value': "22.96", 'image': "aotu.jpg"},
+		 {'id': 7, 'name': "The Bible", 'value': "15.96", 'image': "tb.jpg"},
+		 {'id': 8, 'name': "Fifty Shades of Gray", 'value': "22.96", 'image': "fsog.jpg"},
+		 {'id': 9, 'name': "The Hobbit", 'value': "37.96", 'image': "th.jpg"},
+		 {'id': 10, 'name': "The Boy in Striped Pjamas", 'value': "24.96", 'image': "tbisp.jpg"},
+		 {'id': 11, 'name': "The Diary of a Young Girl", 'value': "39.96", 'image': "tdoayg.jpg"},
+		 {'id': 12, 'name': "Trump: The Art of the Deal", 'value': "33.96", 'image': "taotd.jpg"},
+		 {'id': 13, 'name': "Ninja: Get Good: My Ultimate Guide to Gaming", 'value': "16.96", 'image': "ggmugtg.jpg"}]
 
 
 @app.route('/')
@@ -21,12 +29,19 @@ def index():
 
 @app.route('/cart')
 def cart():
+	cart, cartQuantity, price_sum = [], [], .0
 	if 'cart' not in session:
-		return rend('cart.html', cart="Cart empty", books=books)
-	cart = []
+		return rend('cart.html', cart="Cart empty", books=books, cartQuantity=[], price_sum=price_sum)
 	for item in session['cart']:
 		cart.append(int(item))
-	return rend('cart.html', cart=cart, books=books)
+	for item in books:
+		if int(item['id']) in cart:
+			cartQuantity.append([item, cart.count(item['id'])])
+	for item in cartQuantity:
+		item.append(float(item[0]['value']) * item[1])
+		price_sum += float(item[0]['value']) * item[1]
+	price_sum = round(price_sum, 3)
+	return rend('cart.html', cart=cart, books=books, cartQuantity=cartQuantity, price_sum=price_sum)
 
 @app.route('/cart/add/<int:ID>')
 def add(ID):
@@ -39,6 +54,28 @@ def add(ID):
 def clear_cart():
 	session.pop('cart', None)
 	return '<head><meta http-equiv="Refresh" content="2; url=/cart"></head>emptied your cart'
+
+@app.route('/remove/<int:ID>')
+def remove(ID):
+	cart = []
+	for item in session['cart']:
+		cart.append(int(item))
+	for item in cart:
+		if item == ID:
+			cart.remove(item)
+			break
+	session['cart'] = cart
+	if len(session['cart']) == 0: 
+		session.pop('cart', None)
+	return '<head><meta http-equiv="Refresh" content="2; url=/cart"></head>removed item from list'
+
+@app.route('/secret')
+def secret():
+	if 'cart' not in session: 
+		session['cart'] = []
+	for item in books: 
+		session['cart'] += str(item['id'])
+	return '<head><meta http-equiv="Refresh" content="0; url=/cart"></head>'
 
 # useless <<<<<<<<
 @app.route('/get')
