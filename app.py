2b922103a0b7429791e537e65236c5b3399cfd39
Kleina13 <ragnarhelgi22@gmail.com>
2019-10-08 09:22:01 +0000

    removed useless and added books to txt

--- app.py
+++ app.py
@@ -2,25 +2,13 @@
 
 from os import urandom
 from flask import Flask, render_template as rend, session, request
-# from pprint import pprint
+from ast import literal_eval
 
 app = Flask(__name__)
 app.secret_key = urandom(13)
 
-books = [{'id': 0, 'name': "The Anarchist Cookbook", 'value': "34.96", 'image': "acb.jpg"},
-		 {'id': 1, 'name': "The Communist Manifesto", 'value': "21.96", 'image': "c_man.jpg"},
-		 {'id': 2, 'name': "Mein Kampf", 'value': "48.96", 'image': "mein_k.jpg"},
-		 {'id': 3, 'name': "Quran", 'value': "20.96", 'image': "Quran.jpg"},
-		 {'id': 4, 'name': "Lord of the Flies", 'value': "46.96", 'image': "lotf.jpg"},
-		 {'id': 5, 'name': "The Gospel of the Flying Spaghetti Monster", 'value': "9.96", 'image': "tgoftfsm.jpg"},
-		 {'id': 6, 'name': "Angels of the Universe", 'value': "22.96", 'image': "aotu.jpg"},
-		 {'id': 7, 'name': "The Bible", 'value': "15.96", 'image': "tb.jpg"},
-		 {'id': 8, 'name': "Fifty Shades of Gray", 'value': "22.96", 'image': "fsog.jpg"},
-		 {'id': 9, 'name': "The Hobbit", 'value': "37.96", 'image': "th.jpg"},
-		 {'id': 10, 'name': "The Boy in Striped Pajamas", 'value': "24.96", 'image': "tbisp.jpg"},
-		 {'id': 11, 'name': "The Diary of a Young Girl", 'value': "39.96", 'image': "tdoayg.jpg"},
-		 {'id': 12, 'name': "Trump: The Art of the Deal", 'value': "33.96", 'image': "taotd.jpg"},
-		 {'id': 13, 'name': "Ninja: Get Good: My Ultimate Guide to Gaming", 'value': "16.96", 'image': "ggmugtg.jpg"}]
+with open('books.txt') as file:
+	books = literal_eval(file.read())
 
 
 @app.route('/')
@@ -76,14 +64,6 @@ def secret():
 	for item in books: 
 		session['cart'] += str(item['id'])
 	return '<head><meta http-equiv="Refresh" content="0; url=/cart"></head>'
-
-# useless <<<<<<<<
-@app.route('/get')
-def get_session():
-	if 'cart' in session:
-		return str(session['cart'])
-	return 'your cart is empty'
-# useless <<<<<<<<
 
 # error <<<<<<<<<<
 @app.errorhandler(404)
